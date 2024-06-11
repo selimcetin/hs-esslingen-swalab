91e49339095a4c14c3d524ba7e25f2adb33b3317
selimcetin <selim-cetin@web.de>
2024-06-11 17:31:29 +0200

    first commit

---
+++ src/main/java/com/swalab/yodelforum/YodelforumApplication.java
@@ -0,0 +1,13 @@
+package com.swalab.yodelforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YodelforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YodelforumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swalab/yodelforum/YodelforumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swalab.yodelforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YodelforumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
